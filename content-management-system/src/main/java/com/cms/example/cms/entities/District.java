195fdfeb499a57e69bde74e84565e75eed0c036c
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2023-12-25 18:11:56 +0600

    entites updated

--- content-management-system/src/main/java/com/cms/example/cms/entities/District.java
+++ content-management-system/src/main/java/com/cms/example/cms/entities/District.java
@@ -1,10 +1,15 @@
 package com.cms.example.cms.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +20,7 @@ import org.antlr.v4.runtime.misc.NotNull;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.util.Date;
+import java.util.List;
 
 
 @Builder
@@ -42,6 +48,14 @@ public class District {
     @NotNull
     @Column(name = "ACTIVE", nullable = false)
     private Boolean active;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DIVISION_ID", nullable = false)
+    private Division division;
+
+    @OneToMany(mappedBy = "district", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Upazila> upazillas;
+
 
     @Column(name = "CREATED_AT", nullable = false)
     @CreationTimestamp
--- content-management-system/src/main/java/com/cms/example/cms/entities/Upazila.java
+++ content-management-system/src/main/java/com/cms/example/cms/entities/Upazila.java
@@ -2,9 +2,12 @@ package com.cms.example.cms.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,6 +44,11 @@ public class Upazila {
     @NotNull
     @Column(name = "ACTIVE", nullable = false)
     private Boolean active;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "DISTRICT_ID", nullable = false)
+    private District district;
 
     @Column(name = "CREATED_AT", nullable = false)
     @CreationTimestamp
