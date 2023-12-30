edc8ac2c5356e96fad885be348a3a81dee61c6b5
suvro47 <sohel.cse.hstu@gmail.com>
2023-12-30 20:47:08 +0600

    hibernet jackson biner issue resolved

--- src/main/java/com/cms/example/cms/feature/geo/GeoController.java
+++ src/main/java/com/cms/example/cms/feature/geo/GeoController.java
@@ -1,11 +1,10 @@
-package com.cms.example.cms.feature;
+package com.cms.example.cms.feature.geo;
 
 import com.cms.example.cms.common.Routes;
 import com.cms.example.cms.entities.District;
 import com.cms.example.cms.entities.Division;
 import com.cms.example.cms.entities.Upazila;
 import com.cms.example.cms.enums.EntityFetchType;
-import com.cms.example.cms.feature.geo.GeoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -22,22 +22,17 @@ public class GeoController {
 
     private final GeoService service;
 
-    @GetMapping(Routes.DIVISION_BY_ID_ROUTE)
-    public ResponseEntity<?> getDivisionById(@PathVariable Long divisionId, @RequestParam(defaultValue = "NO_FETCH") EntityFetchType fetchType) {
-
-        if (EntityFetchType.NO_FETCH.equals(fetchType)) {
-            Optional<Division> division = service.getDivisionById(divisionId);
-            if (division.isPresent()) {
-                return new ResponseEntity<>(division, HttpStatus.OK);
-            }
-        } else {
-            Optional<Division> divisionWithDetails = service.getDivisionDetailsById(divisionId);
-            if (divisionWithDetails.isPresent()) {
-                return new ResponseEntity<>(divisionWithDetails.get().getDistricts(), HttpStatus.OK);
-            }
-        }
-        return new ResponseEntity<>("DATA NO_FOUND", HttpStatus.NOT_FOUND);
-    }
+	@GetMapping(Routes.DIVISION_BY_ID_ROUTE)
+	public ResponseEntity<?> getDivisionById(@PathVariable Long divisionId,
+			@RequestParam(defaultValue = "NO_FETCH") EntityFetchType fetchType) {
+		Division division = service.getDivisionById(divisionId, fetchType);
+		if (Objects.nonNull(division)) {
+			return new ResponseEntity<>(division, HttpStatus.OK);
+		} else {
+			// TODO : throw EntityNotFoundException
+			return new ResponseEntity<>("DATA NO_FOUND", HttpStatus.NOT_FOUND);
+		}
+	}
 
     @GetMapping(Routes.DISTRICT_BY_ID_ROUTE)
     public ResponseEntity<?> getDistrictById(@PathVariable Long districtId, @RequestParam(defaultValue = "NO_FETCH") EntityFetchType fetchType) {
