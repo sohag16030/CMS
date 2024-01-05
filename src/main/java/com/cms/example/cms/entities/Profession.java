0c67f69a73b35940978671868df54e26e2f985a2
Md Sohag <sohag.ali@valmetpartners.com>
2024-01-05 17:21:29 +0600

    updated

---
+++ src/main/java/com/cms/example/cms/entities/Profession.java
@@ -0,0 +1,42 @@
+package com.cms.example.cms.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "PROFESSION")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Profession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PROFESSION_ID")
+    private Long cmsUserId;
+    
+    @Column(name = "IS_ACTIVE", nullable = false)
+    private Boolean isActive;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Column(name = "UPDATED_AT", nullable = false)
+    @UpdateTimestamp
+    private Date updatedAt;
+}
---
+++ src/main/java/com/cms/example/cms/feature/cmsUser/CmsUserService.java
@@ -0,0 +1,31 @@
+package com.cms.example.cms.feature.cmsUser;
+
+import com.cms.example.cms.entities.CmsUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CmsUserService {
+
+    private final CmsUserRepository cmsUserRepository;
+
+    public List<CmsUser> getAllCmsUsers() {
+        return cmsUserRepository.findAll();
+    }
+
+    public Optional<CmsUser> getCmsUserById(Long id) {
+        return cmsUserRepository.findById(id);
+    }
+
+    public CmsUser saveCmsUser(CmsUser cmsUser) {
+        return cmsUserRepository.save(cmsUser);
+    }
+
+    public void deleteCmsUser(Long id) {
+        cmsUserRepository.deleteById(id);
+    }
+}
