8cc4c4f6abecdffd4ae83fed7d5b577b09b8d5d6
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-01-08 00:10:52 +0600

    Userservice should call repo for all associated entities

--- src/main/java/com/cms/example/cms/feature/user/UserService.java
+++ src/main/java/com/cms/example/cms/feature/user/UserService.java
@@ -1,6 +1,14 @@
 package com.cms.example.cms.feature.user;
 
+import com.cms.example.cms.entities.Address;
 import com.cms.example.cms.entities.CmsUser;
+import com.cms.example.cms.entities.District;
+import com.cms.example.cms.entities.Division;
+import com.cms.example.cms.entities.Upazila;
+import com.cms.example.cms.entities.UserRating;
+import com.cms.example.cms.enums.EntityFetchType;
+import com.cms.example.cms.feature.geo.GeoService;
+import com.cms.example.cms.feature.userRating.UserRatingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,16 +22,31 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final GeoService geoService;
+    private final UserRatingService ratingService;
 
     @Transactional
     public CmsUser saveCmsUser(CmsUser cmsUser) {
-        if (cmsUser.getAddresses() != null) {
-            cmsUser.getAddresses().forEach(address -> address.setCmsUser(cmsUser));
+        if (cmsUser.getUserRating() != null) {
+            if (cmsUser.getUserRating().getUserRatingId() != null) {
+               UserRating rating = ratingService.getUserRatingById(cmsUser.getUserRating().getUserRatingId()));
+            }
         }
-
-//        if (cmsUser.getAcademicInfos() != null) {
-//            cmsUser.getAcademicInfos().forEach(academicInfo -> academicInfo.setCmsUser(cmsUser));
-//        }
+        if (cmsUser.getAddresses() != null) {
+            cmsUser.getAddresses().forEach(address -> {
+                if (address.getDivision().getDivisionId() != null) {
+                    address.setDivision(geoService.getDivisionById(address.getDivision().getDivisionId(), EntityFetchType.NO_FETCH));
+                }
+                if (address.getDistrict().getDistrictId() != null) {
+                    address.setDistrict(geoService.getDistrictById(address.getDistrict().getDistrictId(), EntityFetchType.NO_FETCH));
+                }
+                if (address.getUpazila().getUpazilaId() != null) {
+                    address.setUpazila(geoService.getUpazilaById(address.getUpazila().getUpazilaId(), EntityFetchType.NO_FETCH));
+                }
+                address.setCmsUser(cmsUser);
+            });
+        }
         return userRepository.save(cmsUser);
     }
+
 }
