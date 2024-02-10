821cb3b57af25cf2a17d6fa6f54879be44efa0c2
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-02-10 20:41:37 +0600

    # Log4j2 Configuration working

--- src/main/java/com/cms/example/cms/App.java
+++ src/main/java/com/cms/example/cms/App.java
@@ -2,16 +2,16 @@ package com.cms.example.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        System.out.println("======================================================================================================================================================================================");
-        System.out.println("=========================@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ PROJECT STARTED  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=====================");
-        System.out.println("======================================================================================================================================================================================");
-
+        logger.info("========================= @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ PROJECT STARTED  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ =====================");
     }
 
 }
