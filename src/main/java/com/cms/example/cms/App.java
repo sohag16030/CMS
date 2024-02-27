db86d50ba1c92c0fcf14f0820b4faff4ade92481
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-02-27 23:26:54 +0600

    file upload is not working

--- src/main/java/com/cms/example/cms/App.java
+++ src/main/java/com/cms/example/cms/App.java
@@ -1,11 +1,18 @@
 package com.cms.example.cms;
 
+import com.cms.example.cms.feature.userContent.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class);
 
