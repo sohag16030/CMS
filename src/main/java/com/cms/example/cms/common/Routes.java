3c0e2eb979ab0e8a9810685bd30fa36ab93971ba
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-08 11:30:25 +0600

    singular to plural in route

--- src/main/java/com/cms/example/cms/common/Routes.java
+++ src/main/java/com/cms/example/cms/common/Routes.java
@@ -3,9 +3,9 @@ package com.cms.example.cms.common;
 public final class Routes {
 
     //GEO_SERVICES_GET_BY_ID_ROUTES
-    public static final String DIVISION_BY_ID_ROUTE = "/api/division/{divisionId}";
-    public static final String DISTRICT_BY_ID_ROUTE = "/api/district/{districtId}";
-    public static final String UPAZILA_BY_ID_ROUTE = "/api/upazila/{upazilaId}";
+    public static final String DIVISION_BY_ID_ROUTE = "/api/divisions/{divisionId}";
+    public static final String DISTRICT_BY_ID_ROUTE = "/api/districts/{districtId}";
+    public static final String UPAZILA_BY_ID_ROUTE = "/api/upazilas/{upazilaId}";
 
     //GEO_SERVICES_LIST_RETURN_ROUTES
     public static final String DIVISION_LIST_ROUTE = "/api/divisions";
@@ -13,25 +13,25 @@ public final class Routes {
     public static final String UPAZILA_LIST_ROUTE = "/api/upazilas";
 
     //CMS_USER_SERVICES_ROUTES
-    public static final String CMS_USER_SIGN_UP_ROUTE = "/api/cmsUser";
-    public static final String CMS_USER_BY_ID_ROUTE = "/api/cmsUser/{userId}";
-    public static final String CMS_USER_DELETE_BY_ID_ROUTE = "/api/cmsUser/{userId}";
-    public static final String CMS_USER_UPDATE_BY_ID_ROUTE = "/api/cmsUser/{cmsUserId}";
+    public static final String CMS_USER_SIGN_UP_ROUTE = "/api/cmsUsers";
+    public static final String CMS_USER_BY_ID_ROUTE = "/api/cmsUsers/{userId}";
+    public static final String CMS_USER_DELETE_BY_ID_ROUTE = "/api/cmsUsers/{userId}";
+    public static final String CMS_USER_UPDATE_BY_ID_ROUTE = "/api/cmsUsers/{cmsUserId}";
     public static final String CMS_USER_LIST_ROUTE = "/api/cmsUsers";
 
     //USER_CONTENTS_UPLOAD_SERVICES_ROUTES
     public static final String CONTENT_UPLOAD_ROUTE = "/api/contents";
-    public static final String CONTENT_UPDATE_BY_ID_ROUTE = "/api/content/{contentId}";
-    public static final String CONTENT_DOWNLOAD_BY_ID_ROUTE = "/api/content/download/{contentId}";
-    public static final String CONTENT_BY_ID_ROUTE = "/api/content/{contentId}";
+    public static final String CONTENT_UPDATE_BY_ID_ROUTE = "/api/contents/{contentId}";
+    public static final String CONTENT_DOWNLOAD_BY_ID_ROUTE = "/api/contents/download/{contentId}";
+    public static final String CONTENT_BY_ID_ROUTE = "/api/contents/{contentId}";
     public static final String CONTENT_LIST_ROUTE = "/api/contents";
-    public static final String CONTENT_DELETE_BY_ID_ROUTE = "/api/content/{contentId}";
+    public static final String CONTENT_DELETE_BY_ID_ROUTE = "/api/contents/{contentId}";
 
     //AUTH
-    public static final String USER_LOGIN = "/user/accessToken";
-    public static final String USER_ACCESS_TOKEN_FROM_REFRESH_TOKEN = "/user/getAccessTokenFromRefreshToken";
-    public static final String USER_ROLE_ASSIGNMENT = "/user/{userId}/permission";
-    public static final String USER_LOGOUT = "/user/logout";
+    public static final String USER_LOGIN = "/users/accessToken";
+    public static final String USER_ACCESS_TOKEN_FROM_REFRESH_TOKEN = "/users/getAccessTokenFromRefreshToken";
+    public static final String USER_ROLE_ASSIGNMENT = "/users/{userId}/permission";
+    public static final String USER_LOGOUT = "/users/logout";
 
 
 }
